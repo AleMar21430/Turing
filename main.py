d8c8742a4d2e1c54f49fd6deb497604c0e0eda3a
Peko Chama <mar21430@uvg.edu.gt>
2023-11-22 09:43:48 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
+from typing import List
 import yaml
 
-class Turing:
-	def __init__(self):
-		pass
+class YAML_Turing:
+	tape: 'Tape'
+
+	def init (self):
+		self.tape = Tape()
+		self.tape_alphabet
+		self.alphabet
+		self.states
+
+		self.initial_state
+		self.accept_state
+		self.final_states
+
+		self.current_state
+		self.blank_symbol
+		self.delta
 
 class Tape:
-	def __init__(self):
-		pass
+	tape: List[str]
+	head_position: int = 0
+	blank_symbol: str
+
+	def init (self, tape: str, epsilon: str):
+		self.tape = list(tape)
+		self.blank_symbol = epsilon
