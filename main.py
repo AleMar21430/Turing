fd72f0a6d7e07c9f71a047b945a55a2ac65b37b8
Peko Chama <mar21430@uvg.edu.gt>
2023-11-22 09:58:40 -0600

    Commit

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
-from typing import List
+from typing import List, Dict
 import yaml
 
 class YAML_Turing:
-	tape: 'Tape'
+	tape: "Tape"
+	states: str
+	initial_state: str
+	final_states: str
+	accept_state: str
+	alphabet: str
+	tape_alphabet: str
+	blank_symbol: str
+	delta: str
+	current_state: str
 
-	def init (self):
-		self.tape = Tape()
-		self.tape_alphabet
-		self.alphabet
-		self.states
-
-		self.initial_state
-		self.accept_state
-		self.final_states
-
-		self.current_state
-		self.blank_symbol
-		self.delta
 
 class Tape:
 	tape: List[str]
 	head_position: int = 0
 	blank_symbol: str
 
-	def init (self, tape: str, epsilon: str):
-		self.tape = list(tape)
-		self.blank_symbol = epsilon
+with open("machine.yaml", "r") as file: config = yaml.safe_load(file)
+
+machine               = YAML_Turing()
+machine.tape          = None
+machine.delta         = config["delta"]
+machine.states        = config["q_states"]["q_list"]
+machine.alphabet      = config["alphabet"]
+machine.blank_symbol  = config["blank"]
+machine.final_states  = config["q_states"]["final"]
+machine.accept_state  = config["q_states"]["accept"]
+machine.tape_alphabet = config["tape_alphabet"]
+machine.initial_state = config["q_states"]["initial"]
+machine.current_state = machine.initial_state
